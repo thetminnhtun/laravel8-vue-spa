4c244eccc28f4c1a7bf6a6d6ed46165f45464be2
Thet Minn Htun <thetminnhtun92@gmail.com>
2020-09-21 19:50:31 +0630

    4_laravel_8_validation

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -14,6 +14,15 @@ class ProductController extends Controller
 
     public function store(Request $request)
     {
+        $request->validate([
+            'name' => 'required|string',
+            'price' => 'required|numeric'
+        ], [
+            'name.required' => 'အမည်ထည့်ရန်လိုအပ်သည်။',
+            'name.string' => 'အမည်သည် စာသားဖြစ်ရမည်။',
+            'price.required' => 'စေ◌ျးနိှုန်းထည့်ပါ။'
+        ]);
+
         $product = Product::create($request->only('name', 'price'));
         return $product;
     }
@@ -26,6 +35,15 @@ class ProductController extends Controller
 
     public function update(Request $request, $id)
     {
+        $request->validate([
+            'name' => 'nullable|string',
+            'price' => 'nullable|numeric'
+        ], [
+            'name.string' => 'အမည်သည် စာသားဖြစ်ရမည်။',
+            'price.numeric' => 'စေ◌ျးနိှုန်း သည် ဂဏန်းဖြစ်ရမည်။'
+        ]);
+
+
         $product = Product::find($id);
         $product->update($request->only('name', 'price'));
         return $product;
