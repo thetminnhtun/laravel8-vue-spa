ba3b130021bdabfa78fa5f6a0d89024023be135b
Thet Minn Htun <thetminnhtun92@gmail.com>
2020-09-26 16:09:47 +0630

    7 Install Vue in Laravel 8

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Vue Component</div>
+
+                    <div class="card-body">
+                        Vue SPA
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
